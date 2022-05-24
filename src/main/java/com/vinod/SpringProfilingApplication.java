aa1fe8ba499ecf8630573f2e3d6c56a6595c9f39
vinod <borse002@gmail.com>
2022-05-24 21:20:46 +0530

    Initial repository commit

---
+++ src/main/java/com/vinod/SpringProfilingApplication.java
@@ -0,0 +1,13 @@
+package com.vinod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfilingApplication.class, args);
+	}
+
+}
